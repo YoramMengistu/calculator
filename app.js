c9c8eccd7b4e0f37c9b4612f571e679302bc9932
YoramMengistu <mengistuyoram@gmail.com>
2023-04-04 20:54:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const userInput = document.querySelector("#userInput");
+let expression = "";
+
+const press = (num) => {
+  expression += num;
+  userInput.value = expression;
+};
+
+const equal = () => {
+  userInput.value = eval(expression);
+  expression = "";
+};
+const erase = () => {
+  expression = "";
+  userInput.value = expression;
+};
